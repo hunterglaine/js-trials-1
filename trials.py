5f3966b5dc8eb19e8efb35cddb20bd0e7c7e5117
Hunter Laine <hunterglaine@gmail.com>
2021-01-30 14:33:52 -0800

    Translate truncate to Python

--- trials.py
+++ trials.py
@@ -133,8 +133,19 @@ def longest_word_length(words):
 
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    """Truncate repeating characters into one character.
+    
+    >>> truncate('aaaabbbbcccca')
+    'abca'
+    """
 
+    result = []
+
+    for char in string:
+        if len(result) == 0 or char != result[len(result) - 1]:
+            result.append(char)
+
+    return ''.join(result)
 
 def has_balanced_parens(string):
     pass  # TODO: replace this line with your code
